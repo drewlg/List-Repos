73fc5ad4aa13d0acb37795b23fc3a3fbe2ebd1e4
Antony <agd10@hotmail.com>
2018-02-11 00:30:29 -0500

    Add files via upload

---
+++ ListRepos.py
@@ -0,0 +1,44 @@
+
+import requests as rq
+import click
+
+@click.command()
+@click.option('--user_name', prompt='\nEnter Github user name (Default:PyAntony)', help='Github Username. Default:PyAntony')
+
+
+def main(user_name):
+    
+    """Simple program that lists USER_NAME Github repositories."""
+    
+    num = 0
+    repo_list = list()
+
+    # Loop for pagination: iterate over every page and store repository names
+    while True: 
+    
+        GitURL = 'https://api.github.com/users/{}/repos?page={}'.format(user_name, num)
+        r = rq.get(GitURL)
+        
+        try:
+            repo_list = repo_list + [dc['name'] for dc in r.json()] 
+        except:
+            # API daily limit exceeded 
+            print('\n', r, '\n', r.json()['message'])
+            exit()
+            break
+    
+        if len(r.json()) == 0:
+            break
+    
+        num += 1
+  
+    repo_num = len(set(repo_list))
+    click.echo('Number of repos: {}\n'.format(repo_num))
+
+    for repo in set(repo_list):
+        click.echo(repo)
+
+if __name__ == '__main__':
+    main()
+        
+
